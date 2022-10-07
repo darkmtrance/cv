6c48c9005aff79bedd284cae86aabf4c85ded34f
michael tomaylla <pulser2310@gmail.com>
2022-10-06 22:15:45 -0500

    nuevo cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+import compress from "astro-compress";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+}), compress(), purgecss()]
 });
