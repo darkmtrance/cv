141f978c015c10b7853d72bbdfc036872d7a8007
michael tomaylla <pulser2310@gmail.com>
2022-10-08 19:49:44 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
 import compress from "astro-compress";
 
 import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-})]
+  integrations: [],
+  vite: {
+    ssr: {
+      external: ["@11ty/eleventy-img"],
+    },
+  },
 });
