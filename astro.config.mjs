1894c80d88b59353e3aae3590dda67f3e38d7b6d
michael tomaylla <pulser2310@gmail.com>
2022-10-23 20:22:17 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), compress()],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img"]
