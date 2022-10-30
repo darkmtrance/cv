fb29a4406f2470958eaaeb3f4d629c313463f9ad
michael tomaylla <pulser2310@gmail.com>
2022-10-29 23:13:00 -0500

    purge

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), compress()],
+  integrations: [react(), compress(), purgecss()],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img"]
